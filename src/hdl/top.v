6bdf3378ede8a3295978243e64428c7a81e81d6c
Arthur Brown <artvvb@gmail.com>
2017-11-20 09:52:22 -0800

    Documented ./src/hdl/top.v

--- src/hdl/top.v
+++ src/hdl/top.v
@@ -21,31 +21,44 @@
 
 
 module top (clk_p, clk_n, col, row, led);
-    input wire clk_p;
-    input wire clk_n;
-    input wire [4:0] col;
-    output wire [4:0] row;
-    output reg [15:0] led;
-    reg [2:0] rowcount = 0;
-    reg [17:0] count = 0;
-    reg [24:0] btn = 0;
-    wire clk;
-    clk_wiz_0 wiz (.clk_in1_p(clk_p), .clk_in1_n(clk_n), .clk_out1(clk));
+    input wire clk_p; // differential positive input clock
+    input wire clk_n; // differential negative input clock
+    input wire [4:0] col; // column state output from keypad
+    output wire [4:0] row; // row input to keypad
+    output reg [15:0] led = 0; // leds to display keypad button state
+    
+    wire clk; // 200 MHz single-ended clock
+    clk_wiz_0 wiz (
+        .clk_in1_p(clk_p),
+        .clk_in1_n(clk_n),
+        .clk_out1(clk)
+    );
+    
+    localparam CLOCKS_PER_ROW = 200000;
+    reg [2:0] rowcount = 0; // index of the row to check, 0-4, increment 1 per millisecond
+    reg [17:0] count = 0; // counter to generate a 1kHz strobe
+    reg [24:0] btn = 0; // last state of each button
     always@(posedge clk)
-        if (count == 199999) begin
+        if (count >= CLOCKS_PER_ROW-1) begin
             count <= 0;
-            if (rowcount >= 4) begin
+            if (rowcount >= 4)
                 rowcount <= 0;
-                led <= btn[15:0];
-            end else
+            else
                 rowcount <= rowcount + 1;
         end else begin
             count <= count + 1;
-            if (count == 99999)
-                btn <= {btn[19:0], col};
+            if (count == CLOCKS_PER_ROW/2-1) // halfway through count period
+                btn <= {btn[19:0], col}; // capture the state of each button in the indexed row
         end
+    
     genvar i;
     generate for (i=0; i<5; i=i+1) begin : IDX
-        assign row[i] = (rowcount == i) ? 0 : 1'bZ;
+        assign row[i] = (rowcount == i) ? 0 : 1'bZ; // hold row pins that are not currently being tested high impedance to avoid short circuits
     end endgenerate
+    
+    // the output method could use some work to be able to display the status of the entire keypad, instead of only 16 buttons
+    // at end of full capture of button state, output states of buttons R0C0:R0C4, R1C0:R1C4, R2C0:R2C4, R3C0
+    always@(posedge clk)
+        if (count >= 199999 && rowcount >= 4)
+            led <= btn[15:0];
 endmodule
